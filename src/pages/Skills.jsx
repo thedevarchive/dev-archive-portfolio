e54ec37b47cdf4e371022c06c4d00b4b78204fd3
Alyssa Ngo <amaengo@protonmail.com>
2025-03-28 16:18:36 +1000

    Completed Skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { FaBitbucket, FaGitAlt, FaGithub, FaJs, FaJava, FaReact, FaNodeJs, FaCogs } from "react-icons/fa"; // Import icons
-import { SiPython } from "react-icons/si"; // Additional icons from react-icons
+import { SiMysql, SiOracle, SiPython, SiSqlite } from "react-icons/si"; // Additional icons from react-icons
 import { TbBrandCSharp, TbLayersIntersect } from "react-icons/tb";
 import { RiRestTimeLine } from "react-icons/ri";
 import { MdDataObject } from "react-icons/md";
 import { FcFlowChart } from "react-icons/fc";
 import { BiLogoVisualStudio } from "react-icons/bi";
-import { DiVisualstudio } from "react-icons/di";
+import { DiMsqlServer, DiVisualstudio } from "react-icons/di";
 import { SiEclipseide } from "react-icons/si";
 import { VscAzureDevops } from "react-icons/vsc";
 
-import "../Skills.css"; 
+import "../Skills.css";
 
 const skills = [
     {
@@ -33,19 +33,20 @@ const skills = [
     {
         category: "IDEs",
         items: [
-            { name: "VS Code", proficiency: "intermediate", icon: <BiLogoVisualStudio />},
+            { name: "VS Code", proficiency: "intermediate", icon: <BiLogoVisualStudio /> },
             { name: "Visual Studio", proficiency: "intermediate", icon: <DiVisualstudio /> },
-            { name: "Eclipse", proficiency: "intermediate", icon: <SiEclipseide /> }, 
-            { name: "Netbeans", proficiency: "beginner", icon: <TbLayersIntersect /> }, 
+            { name: "Eclipse", proficiency: "intermediate", icon: <SiEclipseide /> },
+            { name: "Netbeans", proficiency: "beginner", icon: <TbLayersIntersect /> },
         ],
     },
     {
         category: "Database Management",
         items: [
-            { name: "RESTful API", proficiency: "intermediate", icon: <RiRestTimeLine /> },
-            { name: "OOP", proficiency: "advanced", icon: <MdDataObject /> },
-            { name: "Algorithms", proficiency: "intermediate", icon: <FcFlowChart /> }, 
-            { name: "Design Patterns", proficiency: "beginner", icon: <TbLayersIntersect /> }, 
+            { name: "Oracle SQL", proficiency: "intermediate", icon: <SiOracle /> },
+            { name: "PL/SQL", proficiency: "beginner", icon: <SiOracle /> },
+            { name: "MSSQL Server", proficiency: "intermediate", icon: <DiMsqlServer /> },
+            { name: "SQLite", proficiency: "beginner", icon: <SiSqlite /> },
+            { name: "MySQL", proficiency: "intermediate", icon: <SiMysql /> },
         ],
     },
     {
@@ -53,8 +54,8 @@ const skills = [
         items: [
             { name: "GitHub", proficiency: "intermediate", icon: <FaGithub /> },
             { name: "Git", proficiency: "beginner", icon: <FaGitAlt /> },
-            { name: "Bitbucket", proficiency: "beginner", icon: <FaBitbucket />}, 
-            { name: "Azure DevOps", proficiency: "advanced", icon: <VscAzureDevops /> }, 
+            { name: "Bitbucket", proficiency: "beginner", icon: <FaBitbucket /> },
+            { name: "Azure DevOps", proficiency: "advanced", icon: <VscAzureDevops /> },
         ],
     },
     {
@@ -62,8 +63,8 @@ const skills = [
         items: [
             { name: "RESTful API", proficiency: "intermediate", icon: <RiRestTimeLine /> },
             { name: "OOP", proficiency: "advanced", icon: <MdDataObject /> },
-            { name: "Algorithms", proficiency: "intermediate", icon: <FcFlowChart /> }, 
-            { name: "Design Patterns", proficiency: "beginner", icon: <TbLayersIntersect /> }, 
+            { name: "Algorithms", proficiency: "intermediate", icon: <FcFlowChart /> },
+            { name: "Design Patterns", proficiency: "beginner", icon: <TbLayersIntersect /> },
         ],
     },
 ];
@@ -73,23 +74,39 @@ export function Skills() {
         <div className="home-container">
             <div className="glass-card">
                 <h2>My Skills</h2>
+                <h3 className="subtitle">
+                    <p>
+                        <span className={`proficiency beginner`}>
+                            ●
+                        </span>
+                        &nbsp; Familiar &emsp; 
+                        <span className={`proficiency intermediate`}>
+                            ●
+                        </span>
+                        &nbsp; Proficient &emsp; 
+                        <span className={`proficiency advanced`}>
+                            ●
+                        </span>
+                        &nbsp; Advanced 
+                    </p>
+                </h3>
                 <div className="skill-container">
-                {skills.map((skillCategory, index) => (
-                    <div key={index} className="skill-category">
-                        <h3>{skillCategory.category}</h3>
-                        <ul>
-                            {skillCategory.items.map((item, idx) => (
-                                <li key={idx} className="skill-item">
-                                    <span className="icon">{item.icon}</span> {/* Icon next to name */}
-                                    {item.name}
-                                    <span className={`proficiency ${item.proficiency}`}>
-                                        ●
-                                    </span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
+                    {skills.map((skillCategory, index) => (
+                        <div key={index} className="skill-category">
+                            <h3>{skillCategory.category}</h3>
+                            <ul>
+                                {skillCategory.items.map((item, idx) => (
+                                    <li key={idx} className="skill-item">
+                                        <span className="icon">{item.icon}</span> {/* Icon next to name */}
+                                        {item.name}
+                                        <span className={`proficiency ${item.proficiency}`}>
+                                            ●
+                                        </span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
